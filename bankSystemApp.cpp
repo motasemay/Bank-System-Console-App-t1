19603f414cec3926584f1e7caa7e47a0bcdd33d4
motasemay <smsmotasem@gmail.com>
2024-12-19 17:48:04 +0200

    adding customerMenu

--- bankSystemApp.cpp
+++ bankSystemApp.cpp
@@ -286,15 +286,66 @@ public:
 };	
 class Customer : public Account{
 public:
-	Customer() {
-
-	}
 	
+	void customerMenu() {
+		int choice = 0;
+		while (choice != 6) {
+			cout << "\nCUSTOMER MENU:"
+				<< "\n1. Display Account's Info"
+				<< "\n2. Deposit money"
+				<< "\n3. Withdraw money"
+				<< "\n4. Update Accounts' info"
+				<< "\n5. View audit file"
+				<< "\n6. stop"
+				<< "\nChoose an option: ";
+
+			cin >> choice;
+
+			switch (choice) {
+			case 1: {
+				DisplayAccountInfo();
+				break;
+			}
+			case 2: {
+				double depositAmount;
+				cout << "\nEnter amount to deposit: ";
+				cin >> depositAmount;
+				deposit(depositAmount);
+				break;
+			}
+			case 3: {
+				double withdrawAmount;
+				cout << "\nEnter amount to withdraw: ";
+				cin >> withdrawAmount;
+				withdraw(withdrawAmount);
+				break;
+			}
+			case 4: {
+				updateAccountInfo();
+				break;
+			}
+			case 5: {
+				DisplayAuditFile();
+				break;
+			}
+			case 6: {
+				cout << "\nExiting customer menu.\n";
+				break;
+			}
+			default: {
+				cout << "\nInvalid choice, select a valid option: ";
+				break;
+			}
+			}
+		}
+	}
+
 
 };
 int main() {
 
 	Customer a;
+	a.customerMenu();
 	a.setUserName("motasem");
 	a.setEmail("est@e@sxample.com");
 	a.deposit(233);
